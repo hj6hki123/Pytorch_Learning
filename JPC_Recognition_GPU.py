822f106ddc013a1d73f35512bca0d6f00be1bfb4
klooom <hj6hki123@gmail.com>
2023-05-20 17:03:33 +0800

    let's go

--- JPC_Recognition_GPU.py
+++ JPC_Recognition_GPU.py
@@ -5,22 +5,23 @@
 
 # In[119]:
 
-
 import os
 import shutil
-
 # 遍歷資料夾中的檔案
-for filename in os.listdir('./hiragana_images'):
+source_dir = './dataset/hiragana_images'
+train_dir = './dataset/train_images'
+os.mkdir(train_dir)
+for filename in os.listdir(source_dir):
     # 判斷檔案是否為 jpg 檔
     if filename.endswith('.jpg'):
         # 取得檔名中的非數字字元
         class_name = os.path.splitext(''.join(filter(lambda x: not x.isdigit(), filename)))[0]
         # 建立目標資料夾
-        target_folder = os.path.join('./my_images', class_name)
+        target_folder = os.path.join(train_dir, class_name)
         if not os.path.exists(target_folder):
             os.mkdir(target_folder)
         # 複製檔案到對應的資料夾中
-        shutil.copy(os.path.join('./hiragana_images', filename), os.path.join(target_folder, filename))
+        shutil.copy(os.path.join(source_dir, filename), os.path.join(target_folder, filename))
 
 # # 模型訓練
 
